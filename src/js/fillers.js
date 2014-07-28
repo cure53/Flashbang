f8da01b3d24faac9cbc111a945361ea09a53c880
tunnelshade <tunnelshade@gmail.com>
2014-07-28 22:57:01 +0530

    Added polyfill for `performance` to make flashbang work in Safari

--- src/js/fillers.js
+++ src/js/fillers.js
@@ -88,3 +88,25 @@ if (!String.prototype.padRight) {
     return s;
   };
 }
+
+// Polyfill for performance
+(function() {
+    if(window.performance && window.performance.now) return;
+    if(!window.performance) window.performance = {};
+    var methods = ['webkitNow', 'msNow', 'mozNow'];
+    for(var i = 0; i < methods.length; i++) {
+        if(window.performance[methods[i]]) {
+            window.performance.now = window.performance[methods[i]];
+            return;
+        }
+    }
+    if(Date.now) {
+        window.performance.now = function() {
+            return Date.now();
+        };
+        return;
+    }
+    window.performance.now = function() {
+        return +(new Date());
+    };
+})();
