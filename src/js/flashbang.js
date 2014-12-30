41e8988b3fbd2fad26029a8c0eaeef03813948ef
tunnelshade <tunnelshade@gmail.com>
2014-12-31 00:37:08 +0530

    Changed preloading text to spinner for better UI

--- src/js/flashbang.js
+++ src/js/flashbang.js
@@ -108,8 +108,26 @@ function updateStatus(state) {
 
 // Relatively big files which on preloading will give better user exp. on slow internet connection
 function cacheLargeFiles() {
+  var opts = {
+    lines: 8, // The number of lines to draw
+    length: 15, // The length of each line
+    width: 8, // The line thickness
+    radius: 20, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000000', // #rgb or #rrggbb or array of colors
+    speed: 2, // Rounds per second
+    trail: 30, // Afterglow percentage
+    shadow: true, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: '50%', // Top position relative to parent
+    left: '50%' // Left position relative to parent
+  };
   // Start spinner to show loading in progress
-  spinner = new Spinner(opts).spin(document.getElementById("status"));
+  spinner = new Spinner(opts).spin(document.getElementById("preloadSpinner"));
   showElements("preload");
   $.when(
     $.get("../shumway/build/playerglobal/playerglobal.abcs"),
